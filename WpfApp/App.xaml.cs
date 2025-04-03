17882697d00e3aded661bfa5e5ce1767be41a3b0
trunghq54 <156505269+trunghq54@users.noreply.github.com>
2025-04-03 07:24:25 +0700

    cập nhật login để phân quyền. 1. nurse ở đây chỉ có thể xem child, có thể sửa sau nếu muốn. 2. cập nhật mainwindow tại bài của Hậu đưa lên trống. 3. thiết kế lại giao diện một chút ( hối hận vì phí time.)

--- WpfApp/App.xaml.cs
+++ WpfApp/App.xaml.cs
@@ -1,14 +1,11 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using DAL.Models;
 using System.Windows;
+using WpfApp.View;
 
 namespace WpfApp
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public static Account CurrentAccount { get; set; }
     }
-
 }
